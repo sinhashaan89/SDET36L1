3db3c302732f5910750ce1fcfe7f533720be631f
gajes <gajes@LAPTOP-G5KEBRKS>
2022-07-19 11:44:19 +0530

    commited

---
+++ MAVEN_PROJECT/src/test/java/org/mavenproject/pratice/PraticeTest1.java
@@ -0,0 +1,37 @@
+package org.mavenproject.pratice;
+
+import org.testng.annotations.Test;
+
+public class PraticeTest1 {
+	
+
+	@Test
+	public void test1() {
+		
+		
+		String browser=System.getProperty("BROWSER");
+		String url=System.getProperty("URL");
+		String un=System.getProperty("USERNAME");
+		String pwd=System.getProperty("PASSWORD");
+		System.out.println("test1 --> PraticeTest1");
+		System.out.println("Browser --> "+browser);
+		System.out.println("URL --> "+url);
+		System.out.println("username --> "+un);
+		System.out.println("password --> "+pwd);
+		
+	}
+	
+	@Test
+	public void test2() {
+		
+		System.out.println("test2 --> PraticeTest1");
+	}
+	
+	@Test
+	public void test3() {
+		
+		System.out.println("test3 --> PraticeTest1");
+	}
+	
+
+}
---
+++ MAVEN_PROJECT/src/test/java/org/mavenproject/pratice/PraticeTest2.java
@@ -0,0 +1,27 @@
+package org.mavenproject.pratice;
+
+import org.testng.annotations.Test;
+
+public class PraticeTest2 {
+	
+
+	@Test
+	public void test1() {
+		
+		System.out.println("test1 --> PraticeTest2");
+	}
+	
+	@Test
+	public void test2() {
+		
+		System.out.println("test2 --> PraticeTest2");
+	}
+	
+	@Test
+	public void test3() {
+		
+		System.out.println("test3 --> PraticeTest2");
+	}
+	
+
+}
